9e0c54894eeb6a8185f1dea2e830c0f821e46616
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-10-02 17:05:19 +0700

    Add navigation and listen screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-import LoginScreen from './src/features/auth/screens/login-screen';
+import {Navigation} from './src/features/navigation';
 import Providers from './src/providers/providers';
 import React from 'react';
 
 function App(): JSX.Element {
   return (
     <Providers>
-      <LoginScreen />
-      {/* <PauseIcon /> */}
+      <Navigation />
     </Providers>
   );
 }
---
+++ src/configs/screen-config.ts
@@ -0,0 +1,12 @@
+export const screenConfig = {
+  name: {
+    home: 'home',
+    login: 'login',
+    firstLogin: 'firstLogin',
+    splash: 'splash',
+    listen: 'listen',
+    record: 'record',
+    game: 'game',
+    settings: 'settings',
+  },
+};
