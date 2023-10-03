6fbd8067664060c54f883edbbe7b91362fa18b96
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-10-03 14:29:26 +0700

    Update UI navigation bar

--- src/features/navigation/auth-navigator.tsx
+++ src/features/navigation/auth-navigator.tsx
@@ -1,13 +1,13 @@
-import {ListenScreen} from '../listen/screens';
+import LoginScreen from '../auth/screens/login-screen';
 import React from 'react';
+import {SCREEN_NAMES} from '../../constants/screen';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {screenConfig} from '../../configs/screen-config';
 const Stack = createNativeStackNavigator();
 
 export const AuthStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name={screenConfig.name.listen} component={ListenScreen} />
+      <Stack.Screen name={SCREEN_NAMES.login} component={LoginScreen} />
     </Stack.Navigator>
   );
 };
