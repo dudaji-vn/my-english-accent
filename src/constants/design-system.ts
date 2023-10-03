ed53f0a91ae77d155c9177c5e4d90093f6201061
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-10-03 14:28:50 +0700

    Add design constants

---
+++ src/constants/design-system.ts
@@ -0,0 +1,12 @@
+export const COLORS = {
+  shading: '#081226',
+  secondary: '#183773',
+  primary: '#3060BF',
+  highlight: '#4080FF',
+  text: '#333',
+  stroke: '#333',
+  background: '#fafafa',
+  darkerBackground: '#f2f2f2',
+  lighterBackground: '#fcfcfc',
+  error: '#ff4040',
+};
