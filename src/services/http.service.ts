5cdbff6aebbde854b9b6552bbf6ce78429f7e251
Linh <hoquanglinh2710@gmail.com>
2023-10-05 00:21:52 +0700

    Update service from client app

---
+++ src/services/http.service.ts
@@ -0,0 +1,42 @@
+import axios, {AxiosInstance} from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {baseApiUrl} from '../configs/index';
+import {keyStorage} from '../consts';
+
+export class HttpService {
+  instance: AxiosInstance;
+
+  constructor() {
+    this.instance = axios.create({
+      baseURL: baseApiUrl,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    this.instance.interceptors.request.use(
+      async config => {
+        try {
+          const accessToken = await AsyncStorage.getItem(
+            keyStorage.accessToken,
+          );
+
+          if (accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`;
+          }
+        } catch (error) {
+          console.error('Error reading access token:', error);
+        }
+
+        return config;
+      },
+      error => {
+        return Promise.reject(error);
+      },
+    );
+  }
+}
+
+const httpService = new HttpService().instance;
+
+export default httpService;
---
+++ src/styles/common.tsx
@@ -0,0 +1,64 @@
+import {StyleSheet} from 'react-native';
+
+const commonStyles = StyleSheet.create({
+  textNormal: {
+    fontFamily: 'OpenSans',
+    fontWeight: '500',
+    fontSize: 32,
+    lineHeight: 43.58,
+    color: '#333333',
+  },
+  textHightLight: {
+    fontFamily: 'OpenSans-Bold',
+    fontWeight: '600',
+    fontSize: 32,
+    lineHeight: 43.58,
+    color: '#4080FF',
+  },
+  textNote: {
+    color: '#333333',
+    fontWeight: '300',
+    fontSize: 16,
+    fontStyle: 'italic',
+    opacity: 0.6,
+    textAlign: 'right',
+  },
+  paddingHorizontal20: {
+    paddingHorizontal: 20,
+  },
+  paddingLeft20: {
+    paddingLeft: 20,
+  },
+  paddingRight20: {
+    paddingRight: 20,
+  },
+  marginRight4: {
+    marginRight: 4,
+  },
+  marginHorizontal20: {
+    marginHorizontal: 20,
+  },
+  marginBottom16: {
+    marginBottom: 16,
+  },
+  marginBottom32: {
+    marginBottom: 32,
+  },
+  marginBottom60: {
+    marginBottom: 60,
+  },
+  marginBottom80: {
+    marginBottom: 80,
+  },
+  marginBottom160: {
+    marginBottom: 160,
+  },
+  marginTop60: {
+    marginTop: 60,
+  },
+  marginTop160: {
+    marginTop: 160,
+  },
+});
+
+export default commonStyles;
