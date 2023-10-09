40bddf81b11769457ce8031a9580c1d1cb06d329
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-10-09 17:08:56 +0700

    Update modal card ui

--- src/components/modal-card/modal-card.tsx
+++ src/components/modal-card/modal-card.tsx
@@ -1,14 +1,14 @@
-import {HStack, Heading, Text, VStack, View} from 'native-base';
+import {HStack, Heading, Text, VStack} from 'native-base';
 
 import React from 'react';
 
 type Props = {
   children: React.ReactNode;
   title: string;
-  onConfirm: () => void;
-  onCancel: () => void;
+  description?: string;
   confirmButton?: React.ReactNode;
   cancelButton?: React.ReactNode;
+  footer?: React.ReactNode;
 };
 
 export const ModalCard = (props: Props) => {
@@ -18,12 +18,16 @@ export const ModalCard = (props: Props) => {
         <Heading textAlign="center" fontSize="xl" fontWeight="medium">
           {props.title}
         </Heading>
-        <Text textAlign="center">{props.children}</Text>
+        {props?.description && (
+          <Text textAlign="center">{props.description}</Text>
+        )}
+        {props.children}
       </VStack>
-      <HStack space={2} justifyContent="center">
-        {/* {props.cancelButton} */}
+      <HStack space={8} justifyContent="center">
+        {props.cancelButton}
         {props.confirmButton}
       </HStack>
+      {props.footer}
     </VStack>
   );
 };
