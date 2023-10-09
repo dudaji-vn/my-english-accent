e1dae929b05ee7c3d2da06f1bf24a50fd955b6d1
Linh <hoquanglinh2710@gmail.com>
2023-10-09 17:06:19 +0700

    Init listen screen UI

--- src/features/listen/screens/listen-screen.tsx
+++ src/features/listen/screens/listen-screen.tsx
@@ -1,11 +1,406 @@
-import {Text, View} from 'react-native';
+import * as React from 'react';
+import {Dispatch, FC, SetStateAction, useEffect, useState} from 'react';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ImageBackground,
+  Image,
+} from 'react-native';
+import {Route, SceneRendererProps, TabView} from 'react-native-tab-view';
+import AppButton from '../../../components/app-button';
+import CountryCard from '../../../components/country-card';
+import CustomCard from '../../../components/custom-card';
+import CustomTabBar from '../../../components/custom-library/CustomTabBar';
+import Row from '../../../components/row';
+import {colors, keyStorage} from '../../../consts';
+import {IUserRegisterDTO} from '../../../interfaces/api/Auth';
+import {useRootSelector} from '../../../redux/reducers';
+import {authService} from '../../../services/auth.service';
+import {Dimensions} from 'react-native';
 
-import React from 'react';
+var fullWidth = Dimensions.get('window').width;
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useDispatch} from 'react-redux';
+import {setIsAuthenticate} from '../../../redux/reducers/user.reducer';
+import commonStyles from '../../../styles/common';
+import SearchIcon from '../../../components/icons/search-icon';
+import IndividualIcon from '../../../components/icons/individual-icon';
+import CustomTabBarListen from '../components/CustomTabBarListen';
+import {COLORS} from '../../../constants/design-system';
+import IndividualTab from './individual-tab';
 
-export function ListenScreen() {
+interface IRouteProps {
+  jumpTo: (key: string) => void;
+  userData: IUserRegisterDTO;
+  setUserData: Dispatch<SetStateAction<IUserRegisterDTO>>;
+}
+const FirstRoute = (props: IRouteProps) => {
+  const {jumpTo, userData, setUserData} = props;
+
   return (
-    <View>
-      <Text>ListenScreen</Text>
+    <View style={[styles.container]}>
+      <Image
+        style={styles.backgroundImage as any}
+        source={require('../../../assets/images/BgWave.png')}></Image>
+      <Text style={[commonStyles.marginBottom32, commonStyles.textNormal]}>
+        Hi, your
+        <Text style={commonStyles.textHightLight}> full name</Text> is
+      </Text>
     </View>
   );
-}
+};
+
+const SecondRoute = (props: IRouteProps) => {
+  const {jumpTo, userData, setUserData} = props;
+  return (
+    <View style={[styles.container]}>
+      <Image
+        style={styles.backgroundImage as any}
+        source={require('../../../assets/images/BgWave.png')}></Image>
+      <Text
+        style={[
+          commonStyles.marginBottom32,
+          commonStyles.textNormal,
+          commonStyles.textCenter,
+          commonStyles.paddingHorizontal20,
+        ]}>
+        How’s about your
+        <Text style={commonStyles.textHightLight}> nick name</Text> ?
+      </Text>
+
+      <View>
+        <TextInput
+          placeholderTextColor={colors.stroke}
+          maxLength={16}
+          value={userData.displayName}
+          onChangeText={value => {
+            setUserData(prev => {
+              return {
+                ...prev,
+                displayName: value,
+              };
+            });
+          }}
+          placeholder="ex: Jonas Brothers"
+          style={styles.inputText}
+        />
+        <Text style={commonStyles.textNote}>(Limit 16 characters)</Text>
+      </View>
+      <Row rowStyle={[{gap: 16, marginTop: 214}]}>
+        <AppButton
+          onPress={() => jumpTo('first')}
+          type="transparent"
+          title="Back"
+        />
+        <AppButton
+          disabled={!userData.displayName}
+          onPress={() => jumpTo('third')}
+          type="highlight"
+          title="Next"
+        />
+      </Row>
+    </View>
+  );
+};
+
+const ThirdRoute = (props: IRouteProps) => {
+  const {jumpTo, userData, setUserData} = props;
+  return (
+    <View style={[styles.container]}>
+      <Image
+        style={styles.backgroundImage as any}
+        source={require('../../../assets/images/BgWave.png')}></Image>
+      <Text
+        style={[
+          commonStyles.marginBottom32,
+          commonStyles.textNormal,
+          commonStyles.textCenter,
+        ]}>
+        Where’re you
+        <Text style={commonStyles.textHightLight}> come from</Text> ?
+      </Text>
+      <View
+        style={[
+          {
+            gap: 12,
+            alignItems: 'center',
+            flexDirection: 'row',
+            marginBottom: 120,
+          },
+        ]}>
+        <CountryCard
+          isActive={userData.nativeLanguage === 'Korea'}
+          onPress={() => {
+            setUserData(prev => {
+              return {
+                ...prev,
+                nativeLanguage: 'Korea',
+              };
+            });
+          }}
+          country="Korea"
+          source={require('../../../assets/images/KoreanFlagIcon.png')}
+        />
+        <CountryCard
+          isActive={userData.nativeLanguage === 'VietNam'}
+          onPress={() => {
+            setUserData(prev => {
+              return {
+                ...prev,
+                nativeLanguage: 'VietNam',
+              };
+            });
+          }}
+          country="VietNam"
+          source={require('../../../assets/images/VietNamFlagIcon.png')}
+        />
+      </View>
+      <Row rowStyle={{gap: 10}}>
+        <AppButton
+          onPress={() => jumpTo('second')}
+          type="transparent"
+          title="Back"
+        />
+        <AppButton
+          disabled={!userData.nativeLanguage}
+          onPress={() => jumpTo('four')}
+          type="highlight"
+          title="Next"
+        />
+      </Row>
+    </View>
+  );
+};
+const FourRoute = (props: IRouteProps) => {
+  const {jumpTo, userData, setUserData} = props;
+  const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const allRoles = [
+    {
+      title: 'Developer',
+      image: require('../../../assets/images/Dev.png'),
+    },
+    {
+      title: 'Designer',
+      image: require('../../../assets/images/Designer.png'),
+    },
+    {
+      title: 'Others',
+      image: require('../../../assets/images/Other.png'),
+    },
+  ];
+  return (
+    <View style={[styles.container]}>
+      <Image
+        style={styles.backgroundImage as any}
+        source={require('../../../assets/images/BgWave.png')}></Image>
+      <Text style={[commonStyles.marginBottom32, commonStyles.textNormal]}>
+        Your
+        <Text style={commonStyles.textHightLight}> current role</Text> is ?
+      </Text>
+      <Row rowStyle={[{gap: 10, marginBottom: 170, flexDirection: 'row'}]}>
+        {allRoles.map((item, index) => {
+          return (
+            <CustomCard
+              isActive={userData.role === allRoles[index].title}
+              containerStyle={[
+                userData.role === allRoles[index].title
+                  ? {opacity: 1}
+                  : {opacity: 0.6},
+                {flex: 1},
+              ]}
+              onPress={() => {
+                setUserData(prev => {
+                  return {
+                    ...prev,
+                    role: allRoles[index].title,
+                  };
+                });
+              }}
+              key={index}
+              source={item.image}
+              title={item.title}
+            />
+          );
+        })}
+      </Row>
+
+      <Row rowStyle={{gap: 16}}>
+        <AppButton
+          onPress={() => jumpTo('third')}
+          type="transparent"
+          title="Back"
+        />
+        <AppButton
+          disabled={!userData.role}
+          onPress={() => {
+            if (userData) {
+              setIsLoading(true);
+              authService
+                .register(userData)
+                .then(token => {
+                  AsyncStorage.setItem(keyStorage.accessToken, token);
+                  dispatch(setIsAuthenticate(true));
+                })
+                .catch(err => {
+                  console.log(err.message);
+                  console.log(err?.response.data);
+                });
+            }
+          }}
+          type="highlight"
+          title="Save"
+        />
+      </Row>
+    </View>
+  );
+};
+
+const ListenScreen: FC = () => {
+  const [index, setIndex] = useState(0);
+  const user = useRootSelector(x => x.user);
+  const [userData, setUserData] = useState<IUserRegisterDTO>({
+    userId: '',
+    avatar: '',
+    email: '',
+    displayName: '',
+    fullName: '',
+    nativeLanguage: '',
+    role: '',
+  });
+  useEffect(() => {
+    if (user?.email) {
+      setUserData(prev => {
+        return {
+          ...prev,
+          email: user.email,
+          avatar: user.avatar,
+          userId: user.userId,
+        };
+      });
+    }
+  }, [user]);
+
+  const routes = [
+    {key: 'first', title: 'Individual'},
+    {key: 'second', title: 'Group'},
+    {key: 'third', title: 'Favorite'},
+    {key: 'four', title: 'Download'},
+  ];
+
+  const handleIndexChange = (selectedIndex: number) => {
+    setIndex(selectedIndex);
+  };
+
+  const renderScene = <T extends Route>(
+    props: SceneRendererProps & {route: T},
+  ) => {
+    const {route, jumpTo} = props;
+    switch (route.key) {
+      case 'first':
+        return <IndividualTab />;
+      case 'second':
+        return (
+          <SecondRoute
+            userData={userData}
+            setUserData={setUserData}
+            jumpTo={jumpTo}
+          />
+        );
+      case 'third':
+        return (
+          <ThirdRoute
+            userData={userData}
+            setUserData={setUserData}
+            jumpTo={jumpTo}
+          />
+        );
+      case 'four':
+        return (
+          <FourRoute
+            userData={userData}
+            setUserData={setUserData}
+            jumpTo={jumpTo}
+          />
+        );
+    }
+  };
+  return (
+    <View style={[{flex: 1}]}>
+      <Row rowStyle={styles.header}>
+        <Text style={styles.headerText}>Listen</Text>
+        <SearchIcon />
+      </Row>
+      <TabView
+        renderTabBar={props => <CustomTabBarListen {...props} />}
+        navigationState={{index, routes}}
+        renderScene={renderScene}
+        onIndexChange={handleIndexChange}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+  backgroundImage: {
+    position: 'absolute',
+    width: fullWidth,
+    height: 228,
+    top: -80,
+    zIndex: -1,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    position: 'relative',
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 60,
+  },
+  progress: {
+    height: 30,
+    borderRadius: 16,
+    borderWidth: 1,
+    borderColor: colors.highlight,
+    paddingVertical: 4,
+  },
+  progressText: {
+    fontWeight: '400',
+    fontSize: 16,
+    color: colors.highlight,
+    marginHorizontal: 14,
+  },
+  header: {
+    marginLeft: 20,
+    marginRight: 20,
+    opacity: 0.6,
+    color: '#333333',
+    marginTop: 16,
+  },
+  headerText: {
+    height: 40,
+    fontSize: 22,
+    fontWeight: '400',
+    color: COLORS.text,
+  },
+  tabBar: {
+    flexDirection: 'row',
+    paddingTop: StatusBar.currentHeight || 0,
+  },
+  inputText: {
+    fontSize: 32,
+    textAlign: 'center',
+    borderBottomWidth: 1,
+    borderColor: '#CCC',
+    borderStyle: 'dotted',
+    marginBottom: 12,
+  },
+});
+
+export default ListenScreen;
