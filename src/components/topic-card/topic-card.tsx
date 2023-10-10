ddaa5d8d481fb946127780cdbba2d19feca734a9
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-10-10 10:56:53 +0700

    Add topic components

--- src/components/topic-card/topic-card.tsx
+++ src/components/topic-card/topic-card.tsx
@@ -1,6 +1,6 @@
 import {HStack, Image, Progress, Text, VStack, View} from 'native-base';
-import React from 'react';
-import {ImageSourcePropType} from 'react-native';
+import React, {useMemo} from 'react';
+import {ImageSourcePropType, TouchableOpacity} from 'react-native';
 import {COLORS} from '../../constants/design-system';
 export type Topic = {
   name: string;
@@ -12,30 +12,57 @@ export type Topic = {
 type Props = {
   topic: Topic;
   isActive?: boolean;
+  minimalOnInActive?: boolean;
 };
 
 export const TopicCard = (props: Props) => {
+  const showContent = props.isActive
+    ? true
+    : props.minimalOnInActive
+    ? false
+    : true;
+  const numText = useMemo(() => {
+    const unit = props.topic.totalWords > 1 ? 'words' : 'word';
+    const numDisplay =
+      props.topic.totalWords > 99 ? '99+' : props.topic.totalWords;
+    return `${numDisplay} ${unit}`;
+  }, [props.topic.totalWords]);
+
   return (
-    <View
-      borderWidth={1}
-      borderColor={props.isActive ? COLORS.highlight : COLORS.lighterBackground}
-      p={4}
-      rounded="lg"
-      bg={COLORS.lighterBackground}>
-      <HStack space={4}>
-        <Image source={props.topic.image} alt="image" h={10} w={10} />
-        <VStack>
-          <Text fontWeight="medium">{props.topic.name}</Text>
-          <Text fontWeight="normal">{props.topic.totalWords + ' words'}</Text>
-        </VStack>
-      </HStack>
-      <Progress
-        _filledTrack={{
-          bg: COLORS.highlight,
-        }}
-        mt={4}
-        value={(props.topic.numOfAchieved / props.topic.totalWords) * 100}
-      />
-    </View>
+    <TouchableOpacity
+      disabled={props.isActive}
+      // eslint-disable-next-line react-native/no-inline-styles
+      style={{
+        flex: props.minimalOnInActive ? 0 : 1,
+      }}>
+      <View
+        opacity={props.isActive ? 1 : 0.3}
+        borderWidth={1}
+        borderColor={props.isActive ? COLORS.highlight : COLORS.stroke}
+        p={4}
+        rounded="lg"
+        bg={COLORS.background}>
+        <HStack space={4}>
+          <Image source={props.topic.image} alt="image" h={10} w={10} />
+          {showContent && (
+            <VStack>
+              <Text lineHeight="xs" fontWeight="medium" fontSize="xl">
+                {props.topic.name}
+              </Text>
+              <Text lineHeight="xs" fontWeight="normal">
+                {numText}
+              </Text>
+            </VStack>
+          )}
+        </HStack>
+        <Progress
+          _filledTrack={{
+            bg: COLORS.highlight,
+          }}
+          mt={4}
+          value={(props.topic.numOfAchieved / props.topic.totalWords) * 100}
+        />
+      </View>
+    </TouchableOpacity>
   );
 };
