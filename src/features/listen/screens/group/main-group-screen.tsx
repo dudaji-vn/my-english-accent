fa01af6acb5186bd5a7e17db1d7799e174183696
Linh <hoquanglinh2710@gmail.com>
2023-10-11 09:24:05 +0700

    Init main group screen

---
+++ src/features/listen/screens/group/main-group-screen.tsx
@@ -0,0 +1,14 @@
+import {HStack, Text, View} from 'native-base';
+import FilterIcon from '../../../../components/icons/filter-icon';
+import ListUser from '../../components/ListUser';
+import ListGroup from '../../components/ListGroup';
+
+const MainGroupScreen = () => {
+  return (
+    <View marginX={5} marginTop={5}>
+      <ListGroup />
+    </View>
+  );
+};
+
+export default MainGroupScreen;
