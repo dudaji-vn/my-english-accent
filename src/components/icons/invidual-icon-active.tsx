97ae33ea13d1b4920ef15cbc5ed2fd77b7abce7c
Linh <hoquanglinh2710@gmail.com>
2023-10-11 09:13:23 +0700

    Add some icons resource

--- src/components/icons/invidual-icon-active.tsx
+++ src/components/icons/invidual-icon-active.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Svg, {SvgProps, Path} from 'react-native-svg';
 const IndividualIconActive = (props: SvgProps) => (
-  <Svg width={25} height={24} fill="none" {...props}>
+  <Svg width={24} height={24} fill="none" {...props}>
     <Path
       fill="#fff"
       d="M8.5 14a5 5 0 0 0-5 5v2a1 1 0 1 0 2 0h14a1 1 0 1 0 2 0v-2a5 5 0 0 0-5-5h-8ZM12.5 2a5 5 0 1 0 0 10 5 5 0 0 0 0-10Z"
