5eb5ff584d88c8eab9bed3d53b4cfb957cec8e53
Linh <hoquanglinh2710@gmail.com>
2023-10-11 09:24:40 +0700

    Init individual screen

---
+++ src/features/listen/screens/individual/individual-detail-screen.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'native-base';
+
+const InvididualDetailScreen = () => {
+  return (
+    <View marginX={5} marginTop={5}>
+      <Text>Invididual detail</Text>
+    </View>
+  );
+};
+
+export default InvididualDetailScreen;
