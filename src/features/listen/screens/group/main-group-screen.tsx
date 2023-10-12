af43e3da60c8f35f683d4e6601654ad1332baf08
Linh <hoquanglinh2710@gmail.com>
2023-10-12 09:56:12 +0700

    Add button create new group

--- src/features/listen/screens/group/main-group-screen.tsx
+++ src/features/listen/screens/group/main-group-screen.tsx
@@ -1,12 +1,42 @@
-import {HStack, Text, View} from 'native-base';
-import FilterIcon from '../../../../components/icons/filter-icon';
-import ListUser from '../../components/ListUser';
+import {View, Button, HStack, Text} from 'native-base';
 import ListGroup from '../../components/ListGroup';
+import {Dimensions} from 'react-native';
+import {COLORS} from '../../../../constants/design-system';
+import AddIcon from '../../../../components/icons/add-icon';
+import {SCREEN_NAMES} from '../../../../constants/screen';
+import {useNavigation} from '@react-navigation/native';
+import GroupNotFound from './group-notfound';
 
+const fullWidth = Dimensions.get('window').width;
 const MainGroupScreen = () => {
-  return (
+  const navigation = useNavigation<any>();
+  const handleClick = () => {
+    navigation.navigate(SCREEN_NAMES.listeningsNavigator, {
+      screen: SCREEN_NAMES.createGroup,
+    });
+  };
+
+  return true ? (
+    <GroupNotFound />
+  ) : (
     <View marginX={5} marginTop={5}>
-      <ListGroup />
+      <Button
+        onPress={handleClick}
+        marginBottom={5}
+        borderWidth={1}
+        borderColor={COLORS.highlight}
+        width={fullWidth - 40}
+        height={14}
+        _pressed={{bg: '#E6E6E6'}}
+        bg={COLORS.background}>
+        <HStack space={2}>
+          <AddIcon />
+          <Text color={COLORS.highlight}>Create new group</Text>
+        </HStack>
+      </Button>
+      <View>
+        <ListGroup />
+      </View>
     </View>
   );
 };
