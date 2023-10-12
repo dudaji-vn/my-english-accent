53dde7d784090cd27124832aa43ad85652c33930
Linh <hoquanglinh2710@gmail.com>
2023-10-12 15:37:30 +0700

    Add UI listen detail screen

--- src/features/listen/components/ListUser.tsx
+++ src/features/listen/components/ListUser.tsx
@@ -10,15 +10,7 @@ const data = [
   {id: '4', text: 'Item 4'},
   {id: '5', text: 'Item 1'},
   {id: '6', text: 'Item 2'},
-  {id: '7', text: 'Item 3'},
-  {id: '8', text: 'Item 4'},
-  {id: '1', text: 'Item 1'},
-  {id: '9', text: 'Item 2'},
-  {id: '10', text: 'Item 3'},
-  {id: '11', text: 'Item 4'},
-  {id: '12', text: 'Item 2'},
-  {id: '13', text: 'Item 3'},
-  {id: '14', text: 'Item 4'},
+
   // Add more items as needed
 ];
 
