6339b805c3f0b38ce7475c43b8d04fa2f6e2599a
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-10-12 17:03:05 +0700

    Update dictionary type

--- src/features/record/components/sentence-content-card.tsx
+++ src/features/record/components/sentence-content-card.tsx
@@ -2,36 +2,47 @@ import {HStack, Image, Text, VStack} from 'native-base';
 import React from 'react';
 import Highlighter from 'react-native-highlight-words';
 import {COLORS} from '../../../constants/design-system';
+
+import {StyleSheet} from 'react-native';
+import {flagMap} from '../../../configs';
+import {useRootSelector} from '../../../redux/reducers';
 import {Dictionary} from '../../../types/dictionary';
-const VNFlag = require('../../../assets/images/VietNamFlagIcon.png');
 
 type Props = {
   dictionary: Dictionary;
 };
 
 export const SentenceContentCard = ({dictionary}: Props) => {
+  const nativeLanguage = useRootSelector(
+    state => state.user.profile.nativeLanguage,
+  )!;
+
   return (
     <VStack space={1}>
       <HStack alignItems="center" space={4}>
         <Highlighter
-          style={{
-            fontWeight: 400,
-            color: COLORS.text,
-            fontSize: 16,
-          }}
+          style={styles.highlight}
           highlightStyle={{
             color: COLORS.highlight,
           }}
-          searchWords={[dictionary.text]}
-          textToHighlight={dictionary.example}
+          searchWords={[dictionary.text.en]}
+          textToHighlight={dictionary.example.en}
         />
       </HStack>
       <HStack mt={3} alignItems="center" space={2}>
-        <Image w={8} h={8} alt="Korean flag" source={VNFlag} />
+        <Image w={6} h={6} alt="Korean flag" source={flagMap[nativeLanguage]} />
         <Text opacity={0.6} fontSize="md" color={COLORS.text}>
-          {dictionary.exampleVI}
+          {dictionary.example[nativeLanguage]}
         </Text>
       </HStack>
     </VStack>
   );
 };
+
+const styles = StyleSheet.create({
+  highlight: {
+    fontWeight: '600',
+    color: COLORS.text,
+    fontSize: 16,
+  },
+});
