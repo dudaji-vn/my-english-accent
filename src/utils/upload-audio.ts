e4529f204588ebd13ea94ecab52798163bb670e0
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-10-16 11:16:25 +0700

    Add uploadAudio function

---
+++ src/utils/upload-audio.ts
@@ -0,0 +1,19 @@
+export const uploadAudio = async (file: {
+  uri: string;
+  type: string;
+  name: string;
+}) => {
+  const formData = new FormData();
+  formData.append('file', file);
+  formData.append('upload_preset', 'kctmadgr');
+  formData.append('cloud_name', 'deowxipsu');
+  const response = await fetch(
+    'https://api.cloudinary.com/v1_1/deowxipsu/auto/upload',
+    {
+      method: 'post',
+      body: formData,
+    },
+  );
+  const data = await response.json();
+  return data.secure_url;
+};
