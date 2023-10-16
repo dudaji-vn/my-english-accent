3a3427c48f774e800d167fc5abbbb90b8804aa77
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-10-16 11:18:32 +0700

    Ignore log notifications

--- App.tsx
+++ App.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import {Navigation} from './src/features/navigation';
 import Providers from './src/providers/providers';
 import Stores from './src/redux/stores';
+import {LogBox} from 'react-native';
+
+// Ignore log notification by message:
+LogBox.ignoreLogs(['Warning: ...']);
+
+// Ignore all log notifications:
+LogBox.ignoreAllLogs();
 
 function App(): JSX.Element {
   return (
