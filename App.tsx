06dd6a9bf514683bf8cb0319d9d0d07bfde68152
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-10-18 16:59:57 +0700

    Set default native speaker voice

--- App.tsx
+++ App.tsx
@@ -3,12 +3,14 @@ import {Navigation} from './src/features/navigation';
 import Providers from './src/providers/providers';
 import Stores from './src/redux/stores';
 import {LogBox} from 'react-native';
+import Tts from 'react-native-tts';
 
 // Ignore log notification by message:
 LogBox.ignoreLogs(['Warning: ...']);
 
 // Ignore all log notifications:
 LogBox.ignoreAllLogs();
+Tts.setDefaultVoice('en-us-x-iom-local');
 
 function App(): JSX.Element {
   return (
