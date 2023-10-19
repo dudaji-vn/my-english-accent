00deec2c657ee191736ada43cc4ff4acf0376c9a
Linh <hoquanglinh2710@gmail.com>
2023-10-19 14:57:38 +0700

    Integrare api for list audio screen

--- src/features/listen/screens/individual/list-audio-screen.tsx
+++ src/features/listen/screens/individual/list-audio-screen.tsx
@@ -1,21 +1,26 @@
-import {HStack, Text, View} from 'native-base';
-import ScreenWrapper from '../../../../components/layout/screen-wrapper';
-import {Swiper} from '../../../../components/swiper';
-import CustomSwiper from '../../components/CustomSwiper';
-import AudioItem from '../../components/AudioItem';
+import {NavigationProp, RouteProp} from '@react-navigation/native';
+import {HStack, View} from 'native-base';
 import {Dimensions} from 'react-native';
 import {Filter} from '../../../../components/filter';
-import PlayAllIcon from '../../../../components/icons/play-all-icon';
-import CloseIcon from '../../../../components/icons/close-icon';
-import {RouteProp, NavigationProp} from '@react-navigation/native';
-import UserAvatar from '../../../../components/user-avatar';
 import ArrowDownIcon from '../../../../components/icons/arrow-down-icon';
+import CloseIcon from '../../../../components/icons/close-icon';
+import PlayAllIcon from '../../../../components/icons/play-all-icon';
+import ScreenWrapper from '../../../../components/layout/screen-wrapper';
+import UserAvatar from '../../../../components/user-avatar';
+import AudioItem from '../../components/AudioItem';
+import CustomSwiper from '../../components/CustomSwiper';
+import {useQuery} from '@tanstack/react-query';
+import {listenService} from '../../../../services/listen.service';
 const fullHeight = Dimensions.get('window').height;
+import {SwiperFlatList} from 'react-native-swiper-flatlist';
+import {useMemo, useState} from 'react';
 type Props = {
   route: RouteProp<any>;
   navigation: NavigationProp<any>;
 };
+import AudioRecorderPlayer from 'react-native-audio-recorder-player';
 const ListAudioListenScreen = (props: Props) => {
+  const [currentPlayingIndex, setCurrentPlayingIndex] = useState<number>(-1);
   const filterItems = [
     {
       label: 'Test',
@@ -44,12 +49,35 @@ const ListAudioListenScreen = (props: Props) => {
     },
   ];
   const {route} = props;
-  const typeScreen = route.params?.typeScreen;
+  const {typeScreen, recordId} = route.params!;
+  if (!recordId) {
+    return null;
+  }
+  const {data: audioList} = useQuery(['getAudioDetail', recordId], () =>
+    listenService.getAudioList(recordId),
+  );
+  const dataRecord = useMemo(() => {
+    let mergeArray = [];
+    if (!audioList) {
+      return;
+    }
+    const {currentRecord, nextRecord} = audioList!;
+    if (currentRecord) {
+      mergeArray.push(currentRecord);
+    }
+    if (nextRecord && nextRecord.length > 0) {
+      mergeArray = [...mergeArray, ...nextRecord];
+    }
+    return mergeArray.slice(0, 9);
+  }, [audioList]);
+  const playAudio = (index: number) => {
+    setCurrentPlayingIndex(index);
+  };
   return (
     <ScreenWrapper>
       <HStack justifyContent={'space-between'} mb={6}>
         <CloseIcon />
-        {true && (
+        {typeScreen === 'group' && (
           <Filter
             maxHeight={220}
             marginTop={2}
@@ -71,18 +99,16 @@ const ListAudioListenScreen = (props: Props) => {
       </HStack>
 
       <View h={700}>
-        <CustomSwiper>
-          <AudioItem />
-          <AudioItem />
-          <AudioItem />
-          <AudioItem />
-          <AudioItem />
-          <AudioItem />
-          <AudioItem />
-          <AudioItem />
-          <AudioItem />
-          <AudioItem />
-        </CustomSwiper>
+        {dataRecord && dataRecord.length > 0 && (
+          <SwiperFlatList
+            maxToRenderPerBatch={1}
+            initialNumToRender={1}
+            data={dataRecord}
+            renderItem={({item, index}) => (
+              <AudioItem key={index} record={item} />
+            )}
+          />
+        )}
       </View>
     </ScreenWrapper>
   );
