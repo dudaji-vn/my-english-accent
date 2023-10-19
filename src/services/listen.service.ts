a283bf946d6d9c79ba3b398e4661e3a192357af9
Linh <hoquanglinh2710@gmail.com>
2023-10-19 14:55:59 +0700

    Add getListenDetail and getAudioList in listen service

--- src/services/listen.service.ts
+++ src/services/listen.service.ts
@@ -1,5 +1,10 @@
 import {listenEndpoint} from '../configs';
 import {IApiResponse} from '../interfaces/api/Http';
+import {
+  IGetAudioList,
+  IListenDetailResponse,
+  IParamListenDetail,
+} from '../interfaces/api/Listen';
 import {IUserProgress} from '../interfaces/api/User';
 import httpService from './http.service';
 
@@ -7,6 +12,25 @@ class ListenService {
   async getUserProgress(): Promise<IUserProgress[]> {
     const res = await httpService.get<IApiResponse<IUserProgress[]>>(
       listenEndpoint.getUserProgress,
+    );
+
+    return res.data.data;
+  }
+  async getListenDetail(
+    params: IParamListenDetail,
+  ): Promise<IListenDetailResponse[]> {
+    const res = await httpService.get<IApiResponse<IListenDetailResponse[]>>(
+      listenEndpoint.getListenDetail,
+      {
+        params: params,
+      },
+    );
+    return res.data.data;
+  }
+
+  async getAudioList(recordId: string) {
+    const res = await httpService.get<IApiResponse<IGetAudioList>>(
+      `${listenEndpoint.getAudioList}/${recordId}`,
     );
 
     return res.data.data;
