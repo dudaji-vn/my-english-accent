825438d8500fd701b0c56f95ef598c45db2b364e
Linh <hoquanglinh2710@gmail.com>
2023-10-20 09:11:32 +0700

    Update swiper listen to play audio

--- src/features/listen/screens/individual/list-audio-screen.tsx
+++ src/features/listen/screens/individual/list-audio-screen.tsx
@@ -1,26 +1,37 @@
-import {NavigationProp, RouteProp} from '@react-navigation/native';
-import {HStack, View} from 'native-base';
+import {
+  NavigationProp,
+  RouteProp,
+  useNavigation,
+} from '@react-navigation/native';
+import {useQuery} from '@tanstack/react-query';
+import {HStack, Pressable, View} from 'native-base';
+import {useMemo, useRef, useState} from 'react';
 import {Dimensions} from 'react-native';
+import SwiperDeck from 'react-native-deck-swiper';
 import {Filter} from '../../../../components/filter';
 import ArrowDownIcon from '../../../../components/icons/arrow-down-icon';
 import CloseIcon from '../../../../components/icons/close-icon';
 import PlayAllIcon from '../../../../components/icons/play-all-icon';
 import ScreenWrapper from '../../../../components/layout/screen-wrapper';
 import UserAvatar from '../../../../components/user-avatar';
-import AudioItem from '../../components/AudioItem';
-import CustomSwiper from '../../components/CustomSwiper';
-import {useQuery} from '@tanstack/react-query';
 import {listenService} from '../../../../services/listen.service';
+import AudioItem from '../../components/AudioItem';
 const fullHeight = Dimensions.get('window').height;
-import {SwiperFlatList} from 'react-native-swiper-flatlist';
-import {useMemo, useState} from 'react';
 type Props = {
   route: RouteProp<any>;
   navigation: NavigationProp<any>;
 };
-import AudioRecorderPlayer from 'react-native-audio-recorder-player';
+
+import {useDispatch} from 'react-redux';
+import {togglePlayAll} from '../../../../redux/reducers/slider.reducer';
+import {useRootSelector} from '../../../../redux/reducers';
+
 const ListAudioListenScreen = (props: Props) => {
-  const [currentPlayingIndex, setCurrentPlayingIndex] = useState<number>(-1);
+  const swiperRef = useRef<SwiperDeck>(null);
+  const dispatch = useDispatch();
+  const playAll = useRootSelector(item => item.slider.isPlayAll);
+  const navigation = useNavigation();
+
   const filterItems = [
     {
       label: 'Test',
@@ -50,6 +61,7 @@ const ListAudioListenScreen = (props: Props) => {
   ];
   const {route} = props;
   const {typeScreen, recordId} = route.params!;
+
   if (!recordId) {
     return null;
   }
@@ -68,15 +80,22 @@ const ListAudioListenScreen = (props: Props) => {
     if (nextRecord && nextRecord.length > 0) {
       mergeArray = [...mergeArray, ...nextRecord];
     }
-    return mergeArray.slice(0, 9);
+    return mergeArray;
   }, [audioList]);
-  const playAudio = (index: number) => {
-    setCurrentPlayingIndex(index);
+
+  const handleNext = () => {
+    swiperRef.current?.swipeLeft();
   };
   return (
     <ScreenWrapper>
       <HStack justifyContent={'space-between'} mb={6}>
-        <CloseIcon />
+        <Pressable
+          onPress={() => {
+            navigation.goBack();
+          }}>
+          <CloseIcon />
+        </Pressable>
+
         {typeScreen === 'group' && (
           <Filter
             maxHeight={220}
@@ -85,7 +104,7 @@ const ListAudioListenScreen = (props: Props) => {
             placement="bottom"
             icon={
               <HStack space={2} alignItems={'center'}>
-                <UserAvatar nation={'ko'} />
+                <UserAvatar nativeLanguage={'ko'} />
                 <ArrowDownIcon />
               </HStack>
             }
@@ -95,19 +114,62 @@ const ListAudioListenScreen = (props: Props) => {
             filterItems={filterItems}
           />
         )}
-        <PlayAllIcon />
+        <Pressable
+          onPress={() => {
+            dispatch(togglePlayAll());
+          }}>
+          <PlayAllIcon isHighLight={playAll} />
+        </Pressable>
       </HStack>
 
       <View h={700}>
         {dataRecord && dataRecord.length > 0 && (
-          <SwiperFlatList
-            maxToRenderPerBatch={1}
-            initialNumToRender={1}
-            data={dataRecord}
-            renderItem={({item, index}) => (
-              <AudioItem key={index} record={item} />
+          <SwiperDeck
+            ref={swiperRef}
+            horizontalSwipe={false}
+            verticalSwipe={false}
+            infinite={true}
+            swipeAnimationDuration={450}
+            containerStyle={{
+              backgroundColor: 'transparent',
+              paddingHorizontal: 0,
+              paddingVertical: 0,
+              position: 'absolute',
+            }}
+            cardStyle={{
+              top: 0,
+              left: 0,
+            }}
+            cards={dataRecord}
+            renderCard={item => (
+              <AudioItem handleNext={handleNext} record={item} />
             )}
+            backgroundColor="transparent"
+            showSecondCard={false}
           />
+          // <Swiper
+          //   index={currentPlayingIndex}
+          //   ref={swiperRef}
+          //   loadMinimalLoader={<Text>sss</Text>}
+          //   refreshControl={
+          //     <RefreshControl
+          //       refreshing={isFetching}
+          //       colors={[COLORS.highlight]}
+          //     />
+          //   }
+          //   showsButtons={false}
+          //   loop={false}
+          //   showsPagination={false}>
+          //   {dataRecord.map(item => {
+          //     return (
+          //       <AudioItem
+          //         handleNext={handleNext}
+          //         key={item._id}
+          //         record={item}
+          //       />
+          //     );
+          //   })}
+          // </Swiper>
         )}
       </View>
     </ScreenWrapper>
