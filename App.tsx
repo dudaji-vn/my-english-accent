e274fa77fe95b1976cbeb978415478fc23536c34
Linh <hoquanglinh2710@gmail.com>
2023-10-20 09:04:31 +0700

    Add player service and inject it in app

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import Providers from './src/providers/providers';
 import Stores from './src/redux/stores';
 import {LogBox} from 'react-native';
 import Tts from 'react-native-tts';
+import {initializePlayer} from './server/src/services/player.service';
 
 // Ignore log notification by message:
 LogBox.ignoreLogs(['Warning: ...']);
@@ -11,6 +12,7 @@ LogBox.ignoreLogs(['Warning: ...']);
 // Ignore all log notifications:
 LogBox.ignoreAllLogs();
 Tts.setDefaultVoice('en-us-x-iom-local');
+initializePlayer();
 
 function App(): JSX.Element {
   return (
