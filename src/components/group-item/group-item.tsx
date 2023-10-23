428bca2da9047dcb165a8c452bce7cd6b863a5e0
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-10-23 11:37:55 +0700

    Update group item avatar

--- src/components/group-item/group-item.tsx
+++ src/components/group-item/group-item.tsx
@@ -1,7 +1,8 @@
-import {Avatar, HStack, VStack, View} from 'native-base';
+import {Avatar, HStack, Text, VStack, View} from 'native-base';
 import React from 'react';
 import {Group} from '../../types/group';
 import {Text600} from '../text-600';
+import {COLORS} from '../../constants/design-system';
 
 type Props = {
   group: Group;
@@ -12,7 +13,131 @@ export const GroupItem = ({group, rightElement}: Props) => {
     if (group.avatar) {
       return <Avatar source={{uri: group.avatar}} />;
     }
-    return <Avatar>{group.name[0]}</Avatar>;
+    const numOfMembers = group.members.length;
+    switch (numOfMembers) {
+      case 1:
+        return <Avatar>{group.name[0]}</Avatar>;
+      case 2:
+        return (
+          <HStack
+            w={15}
+            space={1}
+            h={15}
+            justifyContent="center"
+            alignItems="center">
+            <Avatar
+              height={7}
+              width={7}
+              source={{uri: group.members[0].avatar}}
+            />
+            <Avatar
+              height={7}
+              width={7}
+              source={{uri: group.members[1].avatar}}
+            />
+          </HStack>
+        );
+      case 3:
+        return (
+          <VStack
+            w={15}
+            space={1}
+            h={15}
+            justifyContent="center"
+            alignItems="center">
+            <Avatar
+              height={7}
+              width={7}
+              source={{uri: group.members[0].avatar}}
+            />
+            <HStack space={1}>
+              <Avatar
+                height={7}
+                width={7}
+                source={{uri: group.members[1].avatar}}
+              />
+              <Avatar
+                height={7}
+                width={7}
+                source={{uri: group.members[2].avatar}}
+              />
+            </HStack>
+          </VStack>
+        );
+      case 4:
+        return (
+          <VStack
+            w={15}
+            space={1}
+            h={15}
+            justifyContent="center"
+            alignItems="center">
+            <HStack space={1}>
+              <Avatar
+                height={7}
+                width={7}
+                source={{uri: group.members[0].avatar}}
+              />
+              <Avatar
+                height={7}
+                width={7}
+                source={{uri: group.members[1].avatar}}
+              />
+            </HStack>
+            <HStack space={1}>
+              <Avatar
+                height={7}
+                width={7}
+                source={{uri: group.members[2].avatar}}
+              />
+              <Avatar
+                height={7}
+                width={7}
+                source={{uri: group.members[3].avatar}}
+              />
+            </HStack>
+          </VStack>
+        );
+      default:
+        return (
+          <VStack
+            w={15}
+            space={1}
+            h={15}
+            justifyContent="center"
+            alignItems="center">
+            <HStack space={1}>
+              <Avatar
+                height={7}
+                width={7}
+                source={{uri: group.members[0].avatar}}
+              />
+              <Avatar
+                height={7}
+                width={7}
+                source={{uri: group.members[1].avatar}}
+              />
+            </HStack>
+            <HStack space={1}>
+              <Avatar
+                height={7}
+                width={7}
+                source={{uri: group.members[2].avatar}}
+              />
+              <Avatar
+                background={COLORS.highlight}
+                justifyContent="center"
+                alignItems="center"
+                height={7}
+                width={7}>
+                <Text color="white" fontSize="xs" textAlign="center">
+                  +{numOfMembers - 3}
+                </Text>
+              </Avatar>
+            </HStack>
+          </VStack>
+        );
+    }
   };
 
   return (
