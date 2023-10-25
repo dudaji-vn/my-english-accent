2b297282721fc020682361386aa2dd658741ef23
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-10-25 11:51:54 +0700

    Add detect no speak feature

--- src/hooks/use-audio-record.tsx
+++ src/hooks/use-audio-record.tsx
@@ -4,12 +4,14 @@ import AudioRecorderPlayer from 'react-native-audio-recorder-player';
 import RNFetchBlob from 'rn-fetch-blob';
 import uuid from 'react-native-uuid';
 const recorder = new AudioRecorderPlayer();
-
+const MIN_METERING = -36;
+const SILENCE_THRESHOLD = -30; // Adjust this value as needed
+const SILENCE_DURATION = 1000;
 export const useAudioRecord = (path?: string) => {
   const [cacheFilePath, setCacheFilePath] = React.useState(path || '');
   const [isPlaying, setIsPlaying] = React.useState(false);
   const [isRecording, setIsRecording] = React.useState(false);
-  const [metering, setMetering] = React.useState(-36);
+  const [metering, setMetering] = React.useState(MIN_METERING);
 
   React.useEffect(() => {
     if (path) {
@@ -28,9 +30,24 @@ export const useAudioRecord = (path?: string) => {
     await recorder.startRecorder(path, undefined, true);
     setIsRecording(true);
 
+    let silenceTimeout: NodeJS.Timeout | null = null;
+
     recorder.addRecordBackListener(e => {
-      const currentMetering = e.currentMetering || -36;
+      const currentMetering = e.currentMetering || MIN_METERING;
       setMetering(currentMetering);
+
+      // Check for silence and start a timer
+      if (currentMetering <= SILENCE_THRESHOLD) {
+        if (!silenceTimeout) {
+          silenceTimeout = setTimeout(stopRecording, SILENCE_DURATION);
+        }
+      } else {
+        // Reset the silence timer if there's sound detected
+        if (silenceTimeout) {
+          clearTimeout(silenceTimeout);
+          silenceTimeout = null;
+        }
+      }
     });
   }
 
@@ -39,7 +56,7 @@ export const useAudioRecord = (path?: string) => {
       const filePath = await recorder.stopRecorder();
       recorder.removeRecordBackListener();
       setCacheFilePath(filePath);
-      setMetering(-36);
+      setMetering(MIN_METERING);
       setIsRecording(false);
     } catch (error) {
       console.log(error);
