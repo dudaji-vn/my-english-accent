b503d8ed617f3cc846f8a32ca65bd1d6a71bbd3e
Linh <hoquanglinh2710@gmail.com>
2023-10-25 09:43:28 +0700

    Update role and category

---
+++ server/src/const/common.ts
@@ -0,0 +1,10 @@
+export const ROLE = {
+  developer: 'developer',
+  designer: 'designer',
+  others: 'others'
+}
+export const Category = {
+  general: 'general',
+  developer: 'developer',
+  designer: 'designer'
+}
