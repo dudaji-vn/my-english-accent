fa93fe13de822ce22f3d87be21b944d6dd712542
Linh <hoquanglinh2710@gmail.com>
2023-10-25 09:49:22 +0700

    Update ui empty data

--- src/components/empty-data/empty-data.tsx
+++ src/components/empty-data/empty-data.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import {Image, Text, View} from 'native-base';
 import {COLORS} from '../../constants/design-system';
+import {ViewProps} from 'react-native';
+import {InterfaceViewProps} from 'native-base/lib/typescript/components/basic/View/types';
+
 const emptyDataImg = require('../../assets/images/empty.png');
 type Props = {
   children?: React.ReactNode;
   title?: string;
-};
+} & InterfaceViewProps;
 
-export const EmptyData = ({children, title = 'List empty'}: Props) => {
+export const EmptyData = ({
+  children,
+  title = 'List empty',
+  ...props
+}: Props) => {
   return (
-    <View justifyContent={'center'} alignItems={'center'}>
+    <View mt={1} justifyContent={'center'} alignItems={'center'} {...props}>
       <Image
         width={200}
         height={200}
@@ -19,7 +26,7 @@ export const EmptyData = ({children, title = 'List empty'}: Props) => {
       <Text mt={5} mb={3} fontSize={20} color={COLORS.highlight}>
         {title}
       </Text>
-      <Text>{children}</Text>
+      {children}
     </View>
   );
 };
